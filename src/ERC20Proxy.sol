40b2a57d5ff001e4d89fdbc6e04466d8fca92ff8
dneilroth <dneilroth@gmail.com>
2024-02-05 21:47:33 -0800

    Add Erc20proxy contract

--- src/ERC20Proxy.sol
+++ src/ERC20Proxy.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @title ERC20Proxy
+/// @notice A proxy contract that distributes frame claim rewards and tracks which farcaster users
+/// have already claimed a frame reward.
 
 contract ERC20Proxy {
     IERC20 public token;
@@ -14,9 +15,14 @@ contract ERC20Proxy {
         token = IERC20(_tokenAddress);
     }
 
-    function sendToken(address to, uint256 amount) public {
-        // Add the `to` address to the mapping
-        recipients[to] = true;
+    error FidAlreadyClaimed();
+
+    function sendToken(uint32 fid, address to, uint256 amount) public {
+        if (recipients[fid] == true) {
+            revert FidAlreadyClaimed();
+        }
+        // Add the farcaster id to the recipients mapping
+        recipients[fid] = true;
 
         // Perform the token transfer
         require(token.transfer(to, amount), "Token transfer failed");
