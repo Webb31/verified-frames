0fc674af9873618e1faea668da9e2e7c59360128
dneilroth <dneilroth@gmail.com>
2024-02-06 20:29:17 -0800

    Images

---
+++ app/api/image.tsx
@@ -0,0 +1,41 @@
+import satori from "satori";
+import sharp from 'sharp';
+import * as fs from "fs";
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+const fontPath = join(process.cwd(), 'Roboto-Regular.ttf')
+let fontData = fs.readFileSync(fontPath)
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.query['id'])
+  const svg = await satori(
+    <div style={{ display: 'flex', color: "black" }}>Verified Addresses</div>,
+    {
+      width: 600,
+      height: 400,
+      fonts: [
+        {
+          name: "Roboto",
+          // Use `fs` (Node.js only) or `fetch` to read the font as Buffer/ArrayBuffer and provide `data` here.
+          data: fontData,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    },
+  );
+
+  const randId = uuidv4();
+  // Convert SVG to PNG using Sharp
+  const pngBuffer = await sharp(Buffer.from(svg))
+  .toFormat('png')
+  .toBuffer();
+
+  // Set the content type to PNG and send the response
+  res.setHeader('Content-Type', 'image/png');
+  res.setHeader('Cache-Control', 'max-age=10');
+  res.send(pngBuffer);
+}
